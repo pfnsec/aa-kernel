968f1a5ecf18f194fcc0febee592318b003e3310
Peter Sherman <pfnsec@gmail.com>
2016-05-12 23:55:39 +0100

    Removed unneeded scripts

--- launch_qemu_armv7.sh
+++
@@ -1,2 +0,0 @@
-qemu-system-arm -cpu cortex-a8 -m 512 -kernel kernel.elf -serial stdio -machine integratorcp 
-#qemu-system-aarch64 -cpu cortex-a57 -m 512 -kernel kernel.elf -serial stdio -machine virt -s
